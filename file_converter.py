f8821c086c2a49d0da6553324d76fe76b014f6a9
abdullah <44318011+fx2y@users.noreply.github.com>
2023-09-20 09:28:29 +0700

    Implement a file conversion function that takes the selected files and converts them into plain text format.

---
+++ file_converter.py
@@ -0,0 +1,37 @@
+import os
+from PyPDF2 import PdfReader
+import textract
+
+
+class FileConverter:
+    SUPPORTED_FORMATS = (".pdf", ".txt", ".docx")
+
+    def __init__(self):
+        pass
+
+    def convert_to_text(self, file_path):
+        file_extension = os.path.splitext(file_path)[1]
+        if file_extension == ".pdf":
+            return self._convert_pdf_to_text(file_path)
+        elif file_extension == ".txt":
+            return self._convert_txt_to_text(file_path)
+        elif file_extension == ".docx":
+            return self._convert_docx_to_text(file_path)
+        else:
+            raise ValueError(f"Unsupported file format: {file_extension}")
+
+    def _convert_pdf_to_text(self, file_path):
+        with open(file_path, "rb") as f:
+            pdf_reader = PdfReader(f)
+            text = ""
+            for page_num in range(len(pdf_reader.pages)):
+                page = pdf_reader.pages[page_num]
+                text += page.extract_text()
+            return text
+
+    def _convert_txt_to_text(self, file_path):
+        with open(file_path, "r") as f:
+            return f.read()
+
+    def _convert_docx_to_text(self, file_path):
+        return textract.process(file_path).decode("utf-8")
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import tkinter as tk
 from file_selector import FileSelector
+from file_converter import FileConverter
 
 root = tk.Tk()
+
+# Create file selector UI
 file_selector = FileSelector(root)
 file_selector.create_file_selector_ui()
+
+# Create file converter
+file_converter = FileConverter()
+
+
+# Create file conversion UI
+def convert_files():
+    file_paths = file_selector.get_file_paths()
+    texts = [file_converter.convert_to_text(file_path) for file_path in file_paths]
+    print(texts)
+
+
+convert_button = tk.Button(
+    root,
+    text="Convert Files",
+    command=convert_files
+)
+convert_button.pack(side=tk.LEFT, padx=5, pady=5)
+
 root.mainloop()
-
-# To get the selected file paths:
-file_paths = file_selector.get_file_paths()
-print(file_paths)
-
-# To check if a file path is in a supported format:
-file_path = "example.pdf"
-is_supported = file_selector.is_supported_format(file_path)
-print(is_supported)
